59590aef88f2ab26a7064703ab5ac70dcb4afb12
Koray Tuncer <ktuncerr@gmail.com>
2022-06-08 18:01:22 +0300

    revize işlemleri

--- src/assets/js/haritaYonetim.js
+++ src/assets/js/haritaYonetim.js
@@ -34,7 +34,7 @@ map.on("click", function mapClickListen(e) {
 });
 
 //Api ile il verilerinin çekilmesi
-let ilApi = "https://localhost:44377/api/IletisimBilgileri/Il";
+var ilApi = "https://localhost:44377/api/IletisimBilgileri/Il";
 
 $.ajax({
   url: ilApi,
@@ -44,8 +44,6 @@ $.ajax({
     //İllerin Listelenmesi
     $("#yonetimIl").empty();
     $("#yonetimIl").append('<option value="0">İl Seçiniz</option>');
-    Listele();
-
     $.each(data, function () {
       $("#yonetimIl").append('<option value="' + this.id + '">' + this.sehiradi + "</option>");
     });
@@ -56,8 +54,8 @@ $.ajax({
 
 //Api ile ilçe verilerinin çekilmesi
 $("#yonetimIl").on("change", function () {
-  let ilId = $(this).val();
-  let ilceApi = "https://localhost:44377/api/IletisimBilgileri/Ilce/" + ilId;
+  var ilId = $(this).val();
+  var ilceApi = "https://localhost:44377/api/IletisimBilgileri/Ilce/" + ilId;
 
   $.ajax({
     url: ilceApi,
@@ -127,7 +125,8 @@ $("#kaydetBtn").on("click", function () {
       data: data,
     })
       .done(function (e) {
-        $("#magazaForm")[0].reset();
+        $("#magazaForm")[0].reset()
+        
         toastr["success"]("Kayıt İşlemi Başarılı");
       })
       .fail(function (xhr) {
@@ -136,24 +135,6 @@ $("#kaydetBtn").on("click", function () {
   }
 });
 
-//Silme İşlemleri
-function magazaSil(id) {
-  $.ajax({
-    url: "https://localhost:44377/api/IletisimBilgileri/" + id,
-    method: "DELETE",
-  })
-    .done(function (e) {
-      Listele(id)
-      toastr["success"]("Silme İşlemi Başarılı");
-    })
-    .fail(function (xhr) {
-      console.error(xhr);
-    });
-
-      
-   
-}
-
 //Datatable Listeleme
 function Listele() {
   $.ajax({
@@ -161,23 +142,12 @@ function Listele() {
     method: "GET",
   })
     .done(function (data) {
-      console.log(data)
       $("#magazaListele").DataTable({
         language: {
           url: "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Turkish.json",
-        }, 
+        },
         data: data,
-        columns: [
-          { data: "il" },
-          { data: "id" },
-          { data: "ilce" },
-          { data: "magzaAdi" },
-          { data: "acikAdres" },
-          { data: "telefon" },
-          { data: "enlem" },
-          { data: "boylam" },
-
-        ],
+        columns: [{ data: "id" }, { data: "il" }, { data: "ilce" }, { data: "magzaAdi" }, { data: "acikAdres" }, { data: "telefon" }, { data: "enlem" }, { data: "boylam" }],
         columnDefs: [
           {
             targets: [0, 1, 2, 3, 4, 5, 6, 7],
@@ -198,37 +168,18 @@ function Listele() {
     });
 }
 
+//Silme İşlemleri
+function magazaSil(id) {
+  $.ajax({
+    url: "https://localhost:44377/api/IletisimBilgileri/" + id,
+    method: "DELETE",
+  })
+    .done(function (e) {
+      toastr["success"]("Silme İşlemi Başarılı");
+    })
+    .fail(function (xhr) {
+      console.error(xhr);
+    });
+}
 
-// function Listele() {
-//   $.ajax({
-//     url: "https://localhost:44377/api/IletisimBilgileri",
-//     method: "GET",
-//     contentType: "application/json",
-//     dataType: "json",
-//   }).done(function (data) {
-//     data.forEach((data) => {
-//       $("#magazaListele").append(`
-    
-//     <tr>
-//       <td scope="row">${data.id}</td>
-//       <td>${data.il}</td>
-//       <td>${data.ilce}</td>
-//       <td>${data.magzaAdi}</td>
-//       <td>${data.acikAdres}</td>
-//       <td>${data.telefon}</td>
-//       <td>${data.enlem}</td>
-//       <td>${data.boylam}</td>
-//       <td style="width:120px ;">   
-//          <a href="" type="button" class="btn btn-success">
-//              <i class="bi bi-pencil-square"></i>
-//          </a>
-//          <a onclick="magazaSil(${data.id})" type="button" class="btn btn-danger">
-//              <i class="bi bi-trash"></i>
-//          </a>
-//       </td>
-//   </tr>   
-    
-//     `);
-//     });
-//   });
-// }
+Listele();
