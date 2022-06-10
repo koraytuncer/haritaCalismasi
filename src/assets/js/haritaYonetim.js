77b65cb060b449b8723d7e1df1f287020751c524
koraytuncer <ktuncerr@gmail.com>
2022-06-10 15:54:17 +0300

    hata giderme işlemi tamamlandı

--- src/assets/js/haritaYonetim.js
+++ src/assets/js/haritaYonetim.js
@@ -38,9 +38,9 @@ let ilceApi = "https://localhost:44377/api/IletisimBilgileri/Ilce/";
 let magazaApi = "https://localhost:44377/api/IletisimBilgileri/";
 
 //Fonksiyonlar
-function illeriGetir() {
+function illeriGetir(api) {
   $.ajax({
-    url: ilApi,
+    url: api,
     method: "GET",
   })
     .done(function (data) {
@@ -48,7 +48,9 @@ function illeriGetir() {
       $("#yonetimIl").empty();
       $("#yonetimIl").append('<option value="0">İl Seçiniz</option>');
       $.each(data, function () {
-        $("#yonetimIl").append('<option value="' + this.id + '">' + this.sehiradi + "</option>");
+        $("#yonetimIl").append(
+          '<option value="' + this.id + '">' + this.sehiradi + "</option>"
+        );
       });
     })
     .fail(function (xhr) {
@@ -66,7 +68,9 @@ function ilceleriGetir(api) {
         $("#yonetimIlce").empty();
         $("#yonetimIlce").append('<option value="0">İlçe Seçiniz</option>');
         $.each(data, function () {
-          $("#yonetimIlce").append('<option value="' + this.id + '">' + this.ilceadi + "</option>");
+          $("#yonetimIlce").append(
+            '<option value="' + this.id + '">' + this.ilceadi + "</option>"
+          );
         });
       })
       .fail(function (xhr) {
@@ -106,6 +110,7 @@ function veriSil(id) {
       console.error(xhr);
     });
 }
+
 function datatableListele(api) {
   $.ajax({
     url: api,
@@ -185,11 +190,26 @@ function magazaListele() {
     <td>${element.magzaAdi}</td>
     <td>${element.acikAdres}</td>
     <td>${element.telefon}</td>
-    <td>${element.enlem}</td>
-    <td>${element.boylam}</td>
+    <td>${parseFloat(element.enlem).toFixed(6)}</td>
+    <td>${parseFloat(element.boylam).toFixed(6)}</td>
     <td>
-    <button class="btn btn-sm btn-success" onclick="veriSil(${element.id})">Sil</button>
-    <a onclick="" class="btn btn-success">Düzenle</a>
+    <button type="button" class="btn btn-sm btn-outline-success" onclick="veriGuncelle(${element.id})">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+            </svg>
+                  <span class="visually-hidden">Button</span>
+             
+                </button>
+         
+         <button type="button" class="btn btn-outline-danger btn-sm" onclick="veriSil(${element.id})">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                  </svg>
+                  <span class="visually-hidden">Button</span>
+                </button>
+         
     </td>
     </tr>
     
